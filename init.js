71b244c9ddd901b824baaf7643a6e1ffefedd030
cesar686 <65380228+cesar686@users.noreply.github.com>
2021-06-24 00:29:47 -0500

    Create init.js

---
+++ init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCoyxXnPfOzBGONahuubEjaou7knVPjHAU",
+    authDomain: "notiupiicsa-ac2c4.firebaseapp.com",
+    projectId: "notiupiicsa-ac2c4",
+    storageBucket: "gs://notiupiicsa-ac2c4.appspot.com",
+    messagingSenderId: "309969353083",
+    appId: "1:309969353083:web:cca6b9dfd810f971a4e5de"
+  });
